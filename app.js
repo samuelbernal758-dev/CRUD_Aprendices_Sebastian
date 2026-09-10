a0f12eb44cdd810ef348579689a1cb2508e02867
williamestupinansilva-oss <williamestupinansilva@gmail.com>
2026-09-10 17:39:32 -0500

    token

--- app.js
+++ app.js
@@ -279,21 +279,25 @@ app.get("/rutaProtegida", autenticarToken , (req, res) => {
     res.json({mensaje: "Este es una ruta protegida"})
 })
 
-app.post("/login", (req, res) =>{
-    const {usuario, clave} = req.body
+app.post("/login", (req, res) => {
+    const { usuario, clave } = req.body
     const usuariobd = {
         "usuario": "jhonny",
         "clave": "abc123"   
     }
-    if(usuario !== usuariobd.usuario || clave !== usuariobd.clave){
-        res.json({mensaje: "Usuario y/o clave incorrectos. "})
+
+    if (usuario !== usuariobd.usuario || clave !== usuariobd.clave) {
+        return res.json({ mensaje: "Usuario y/o clave incorrectos." })
     }
     const token = jswtoken.sign(
-        process.env.JWT_SECRET,
-        {expiresIn: "1h"}
+        { usuario: usuario }, 
+        process.env.JWT_SECRET, 
+        { expiresIn: "1h" } 
     )
-    res.json({token}) 
+
+    res.json({ token }) 
 })
+
 
 app.use(manejadorErrores)
 
--- middleware/autenticarToken.js
+++ middleware/autenticarToken.js
@@ -1,15 +1,20 @@
-const jswtoken = require("jsonwebtoken")
+const jwt = require('jsonwebtoken');
 
 const autenticarToken = (req, res, next) => {
-    const token  = req.header("autenticacion")?.split(" ")[1]
-    if(!token){
-        res.status(401).json({error: "Acceso denegado, no provee token."})
+    const token = req.header("autenticacion")?.split(" ")[1];
+    
+    if (!token) {
+        return res.status(401).json({ error: "Acceso denegado, no provee token." });
     }
-    jwt.verify(token, process-env-JWT_SECRET, (error, usuario) => {
-        if (error) res.status(403).json({Error: "Token Invalido"});
+
+    jwt.verify(token, process.env.JWT_SECRET || "frasesecreta", (error, usuario) => {
+        if (error) {
+            return res.status(403).json({ error: "Token Inválido" });
+        }
+        
         req.usuario = usuario;
-        next();
-    })
+        next(); 
+    });
 }
 
 module.exports = autenticarToken
